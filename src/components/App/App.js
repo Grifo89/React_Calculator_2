68644a75268e543cc1c56cc5e1fdd092eca583e5
Christian Salazar <calaca89@gmail.com>
2020-03-23 13:38:33 -0500

    restore state in app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,10 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '0',
-      total: null,
-      next: null,
-      operation: null
+      input: '0'
     };
   }
 

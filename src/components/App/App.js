41cd4ac5ed59a86aa815c0c407d8725a57e7a9bd
Christian Salazar <calaca89@gmail.com>
2020-03-23 13:23:52 -0500

    Change state in app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ButtonPanel from '../ButtonPanel/ButtonPanel';
 import Display from '../Display/Display';
-// import calculate from '../../logic/calculate'
+import calculate from '../../logic/calculate'
 import './App.css';
 
-
+calculate
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       input: '0',
+      total: null,
+      next: null,
+      operation: null
     };
   }
 
